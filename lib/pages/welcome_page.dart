c56f258ba585e38fe777926dcefb9b705ddd59f1
jksan <jksan.jp@gmail.com>
2022-03-13 12:33:51 +0900

    Start building reusable widgets for app_text https://youtu.be/x4DydJKVvQk?t=1118

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/misc/colors.dart';
 import 'package:flutter_cubit/widgets/app_large_text.dart';
+import 'package:flutter_cubit/widgets/app_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -32,8 +34,25 @@ class _WelcomePageState extends State<WelcomePage> {
                 margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
                 child: Row(children: [
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
                       AppLargeText(text: "Trips"),
+                      AppText(
+                        text: "Mountain",
+                        size: 30,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: 250,
+                        child: AppText(
+                          text:
+                              "Mountain hikes give you an incredible sense of freedom along with endurance test",
+                          color: AppColors.textColor2,
+                          size: 14,
+                        ),
+                      )
                     ],
                   )
                 ]),
