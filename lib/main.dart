0f9be0f9b4858d856ec265265397ee88a5bfce41
jksan <jksan.jp@gmail.com>
2022-03-13 12:02:20 +0900

    format

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: Container()
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Container());
   }
 }
