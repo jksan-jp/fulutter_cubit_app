540ef72760bbae654c6aab6587f88ae3c719e674
jksan <jksan.jp@gmail.com>
2022-03-13 12:21:52 +0900

    Building column forthe carousel https://youtu.be/x4DydJKVvQk?t=817

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -21,12 +22,23 @@ class _WelcomePageState extends State<WelcomePage> {
           itemCount: images.length,
           itemBuilder: (_, index) {
             return Container(
-                width: double.maxFinite,
-                height: double.maxFinite,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: AssetImage("img/" + images[index]))));
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("img/" + images[index]))),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(children: [
+                  Column(
+                    children: const [
+                      AppLargeText(text: "Trips"),
+                    ],
+                  )
+                ]),
+              ),
+            );
           }),
     );
   }
